d959ff8832988527241283701bc56ce9ec3ee0ee
Tom <tmwbook@gmail.com>
2017-04-16 19:47:34 -0400

    Added javadoc and comments

--- src/org/DFS.java
+++ src/org/DFS.java
@@ -8,9 +8,17 @@ import java.util.HashSet;
  */
 public class DFS extends PathfindingStrategy {
 
-  HashSet<Point> visited = new HashSet<>();
-  Point startingLoc;
+  HashSet<Point> visited = new HashSet<>(); // Used to prevent stack overflow
+  Point startingLoc; // Used to validate where we are in recursion
 
+  /**
+   * Recursively find the first path that connects start to goal using a DFS algorithm
+   * @implNote this completely ignores any heuristics
+   * @param start: starting Point
+   * @param goal: ending Point
+   * @return the path to get from the original `start` to `goal`
+   * @throws NoPathException
+   */
   @Override
   public ArrayList<Point> execute(Point start, Point goal) throws NoPathException {
     visited.add(start);
