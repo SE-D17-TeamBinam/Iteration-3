b22c867b1e73afa2e217ae91e977f19f89da1b79
Evan Duffy <eduffy2015@spyponders.com>
2017-04-27 21:39:13 -0400

    Fixed my messed up merge conflict resolution

--- src/org/Point.java
+++ src/org/Point.java
@@ -253,35 +253,6 @@ public class Point {
     // test if the object isn't even the same type of class
     if (obj.getClass() != this.getClass())
       return super.equals(obj);
-    Point pobj = (Point) obj;
-    if (this.xCoord == pobj.xCoord && this.yCoord == pobj.yCoord && this.id == pobj.id && this.floor == pobj.floor && this.neighbors.size() == pobj.neighbors.size()){
-      if (this.names != null) {
-        for (String s : this.names) {
-          if (!pobj.names.contains(s))
-            return false;
-        }
-      }
-      else if (this.names != pobj.names){
-        return false;
-      }
-      ArrayList<Integer> ourNeighbors = new ArrayList<Integer>();
-      ArrayList<Integer> theirNeighbors = new ArrayList<Integer>();
-      for (Point p : this.neighbors)
-        ourNeighbors.add(p.id);
-      for (Point p : pobj.neighbors)
-        theirNeighbors.add(p.id);
-      //System.out.println("ID: " + this.id + " -> " + pobj.id + ".  " + ourNeighbors + "  :  " + theirNeighbors);
-
-      for (int i : ourNeighbors){
-        if (!theirNeighbors.contains(i)){
-          return false;
-        }
-      }
-      return true;
-    }
-    return false;
-  }
-
     Point pobj = (Point) obj; // we can now safely assume that obj is a Point and not null
     // test if the primitive attributes are different
     if (pobj.xCoord != this.xCoord || pobj.yCoord != this.yCoord || pobj.id != this.id || pobj.floor != this.floor)
